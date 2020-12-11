f1487ce9e6bb3aa0ef17cde3eb63a94273564f18
wmeikle <wcmeikle@byui.edu>
2020-12-11 14:15:35 -0700

    Final project updates

--- src/Final/Main.java
+++ src/Final/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    // Tell user what program does
+        System.out.print("""
+                This program gets a loan type from the user, along with their
+                estimated credit score, the loan amount they are requesting in 
+                whole dollars, and the length of the loan in years as integer values.
+                It then calculates and prints the apr, monthly minimum payments, and 
+                the total loan amount with interest included.
+                """);
 
         // Ask what type of loan they are inquiring about
 
