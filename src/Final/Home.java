68c63d5f5404a12db60a9724e67c2e393b40528c
Ryan Hill <56138705+xjjeepster@users.noreply.github.com>
2020-12-11 15:46:32 -0700

    update home

--- src/Final/Home.java
+++ src/Final/Home.java
@@ -13,7 +13,10 @@ public class Home extends LoanObject {
     /** Return APR*/
     try {
         // read from file to get creditScore and convert to APR, return apr
+
         Scanner input = new Scanner(file);
+
+
         switch ((300 <== CreditScore && CreditScore <= 629) ? 0: (630 <== CreditScore && CreditScore <= 689) ? 1:
                 (690 <== CreditScore && CreditScore <= 719) ? 2:(720 <== CreditScore && CreditScore <= 850) ? 3: 4)
         case 0:
@@ -35,12 +38,7 @@ public class Home extends LoanObject {
     }
 
     /** Construct a auto loan with specified APR*/
-    public Home (double apr, int LoanAmount, int TimeInYears) {}
-
-    /** Return final loan amount*/
-    public double FinalAmount() {
-        // Using previously inputted variables calculate and return final amount
-    }
+    public Home (apr, int LoanAmount, int TimeInYears) {}
 
     /** Return monthly minimum payment*/
     public double MonthlyPayment(int InitialAmmount, apr, int TimeInYears) {
@@ -48,6 +46,15 @@ public class Home extends LoanObject {
         double monthlyPayment = (InitialAmmount * apr) / (1-Math.pow(1+apr, (-TimeInYears*12)));
         return monthlyPayment;
     }
+
+    /** Return final loan amount*/
+    public double FinalAmount(double monthlyPayment, apr, int TimeInYears) {
+        // Using previously inputted variables calculate and return final amount
+        double finalAmount = ((monthlyPayment / ((apr*100)/12) * (1-(1/Math.pow(1 +(apr*100)/12), (TimeInYears*12)))));
+        return finalAmount;
+    }
+
+
 
     /** Return string description of loan details */
     public String toString () {
